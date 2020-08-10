bb1683a50adb59bbcdea13bdc6d23c71e02b0f01
shekar2639 <shekar2639@gmail.com>
2020-08-10 10:50:23 -0400

    Updates

---
+++ precache-manifest.6cfee6964a2207330ab4847f57db1890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d08effd734b5210f444876b86e2dd62",
+    "url": "/codetest/index.html"
+  },
+  {
+    "revision": "126b8bb0d5650278b01c",
+    "url": "/codetest/static/css/main.58acf445.chunk.css"
+  },
+  {
+    "revision": "628a88535b9787bd3cb9",
+    "url": "/codetest/static/js/2.442b069e.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/codetest/static/js/2.442b069e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126b8bb0d5650278b01c",
+    "url": "/codetest/static/js/main.35a44881.chunk.js"
+  },
+  {
+    "revision": "9ba53c8011033ad64bb2",
+    "url": "/codetest/static/js/runtime-main.724e0ea6.js"
+  }
+]);
